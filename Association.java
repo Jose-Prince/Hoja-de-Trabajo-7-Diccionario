4b646d1db9fb59280defa76e05d74f6dcb904dfb
Jose-Prince <joseprince148mail.com>
2023-03-21 21:12:11 -0600

    BST Implementation
    Se implementa el Arbol Binario de Búsqueda

---
+++ Association.java
@@ -0,0 +1,25 @@
+public class Association<K, V> {
+
+    K idioma1;
+    V idioma2;
+
+    
+
+    public K getIdioma1() {
+        return this.idioma1;
+    }
+
+    public void setIdioma1(K idioma1) {
+        this.idioma1 = idioma1;
+    }
+
+    public V getIdioma2() {
+        return this.idioma2;
+    }
+
+    public void setIdioma2(V idioma2) {
+        this.idioma2 = idioma2;
+    }
+
+
+}
